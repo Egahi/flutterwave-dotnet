e0182f4c45bb3d62e856a557847be335c993a5d1
Egahi S. O <okworisamuel6@gmail.com>
2020-09-16 09:17:26 +0100

    Feature add unit test

--- src/flutterwave-dotnet-test/Apis/TransactionsTests.cs
+++ src/flutterwave-dotnet-test/Apis/TransactionsTests.cs
@@ -1,38 +1,52 @@
 ﻿using Flutterwave.Net;
+using Flutterwave.Net.Utilities;
+using System;
 using Xunit;
 
 namespace flutterwave_dotnet_test.Apis
 {
     public class TransactionsTests
     {
-        private readonly Transactions transactions;
+        private Transactions _transactions;
 
         public TransactionsTests()
         {
             // Get rave secret key from environmental variables
+            var flutterwaveSecretKey = Environment.GetEnvironmentVariable("FlutterwaveSecretKey");
 
-            // GitHub
-            var raveSecretKey = System.Environment.GetEnvironmentVariable("RaveSecretKey",
-                System.EnvironmentVariableTarget.Process);
-
-            // Local - Windows
-            //var raveSecretKey = System.Environment.GetEnvironmentVariable("RaveSecretKey",
-            //    System.EnvironmentVariableTarget.Machine);
-
-            transactions = new Transactions(new FlutterwaveApi(raveSecretKey));
+            _transactions = new Transactions(new FlutterwaveApi(flutterwaveSecretKey));
         }
 
         [Fact]
-        public void GetTransactions_ReturnsAllTransactions()
+        public void GetTransactions_ValidSecretKey_ReturnsAllTransactions()
         {
             // Act
-            var result = transactions.GetTransactions();
+            var result = _transactions.GetTransactions();
 
             // Assert
             Assert.NotNull(result); 
             Assert.IsType<RaveGetTransactionsResponse>(result);
-            Assert.Equal(expected: "success", actual: result.Status);
-            Assert.Equal(expected: "Transactions fetched", actual: result.Message);
+            Assert.Equal(expected: AppConstants.SUCCESS, actual: result.Status);
+            Assert.Equal(expected: AppConstants.TRANSACTION_FETCHED, actual: result.Message);
+        }
+
+
+        [Fact]
+        public void GetTransactions_InValidSecretKey_ReturnsError()
+        {
+            // Arrange
+            var flutterwaveSecretKey = "";
+            _transactions = new Transactions(new FlutterwaveApi(flutterwaveSecretKey));
+
+            // Act
+            var result = _transactions.GetTransactions();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<RaveGetTransactionsResponse>(result);
+            Assert.Equal(expected: AppConstants.ERROR, actual: result.Status);
+            Assert.Equal(expected: AppConstants.INVALID_AUTHORIZATION_KEY, actual: result.Message);
+            Assert.Null(result.Data);
         }
     }
 }
