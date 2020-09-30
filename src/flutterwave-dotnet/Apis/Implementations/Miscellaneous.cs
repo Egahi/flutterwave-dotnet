dacebc22a4a704b211d5d5541aa7cbba18628bcf
Egahi S. O <okworisamuel6@gmail.com>
2020-09-30 08:05:01 +0100

    Feature verify bank account

---
+++ src/flutterwave-dotnet/Apis/Implementations/Miscellaneous.cs
@@ -0,0 +1,27 @@
+﻿using Flutterwave.Net.Utilities;
+
+namespace Flutterwave.Net
+{
+    public class Miscellaneous : IMiscellaneous
+    {
+        private FlutterwaveApi _flutterwaveApi { get; }
+
+        public Miscellaneous(FlutterwaveApi flutterwaveApi)
+        {
+            _flutterwaveApi = flutterwaveApi;
+        }
+
+        /// <summary>
+        /// Verify a bank account number
+        /// </summary>
+        /// <param name="accountNumber">Account number to be verified</param>
+        /// <param name="bankCode">Bank code. It is returned in the Get banks call as data.code</param>
+        /// <returns>The bank account name and number</returns>
+        public VerifyBankAccountResponse VerifyBankAccount(string accountNumber, string bankCode)
+        {
+            var data = new VerifyBankAccountRequest(accountNumber, bankCode);
+
+            return _flutterwaveApi.Post<VerifyBankAccountResponse>(Endpoints.BANK_ACCOUNT_VERIFICATION, data);
+        }
+    }
+}
