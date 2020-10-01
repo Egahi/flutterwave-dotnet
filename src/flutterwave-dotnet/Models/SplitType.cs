749dd19096c59586902012640dd1e9c64e58a898
Egahi S. O <okworisamuel6@gmail.com>
2020-10-01 23:54:54 +0100

    feature creete sub account

---
+++ src/flutterwave-dotnet/Models/SplitType.cs
@@ -0,0 +1,21 @@
+﻿using Flutterwave.Net.Utilities;
+using System.ComponentModel;
+
+namespace Flutterwave.Net
+{
+    public enum SplitType
+    {
+        /// <summary>
+        /// flat
+        /// </summary>
+
+        [Description(AppConstants.SPLIT_TYPE_FLAT)]
+        Flat,
+
+        /// <summary>
+        /// percentage
+        /// </summary>
+        [Description(AppConstants.SPLIT_TYPE_PERCENTAGE)]
+        Percentage
+    }
+}
