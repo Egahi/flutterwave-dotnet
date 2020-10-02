49daf563ddca1fa03be0ddcdec13ab0f795a7e28
Egahi S. O <okworisamuel6@gmail.com>
2020-10-02 19:48:46 +0100

    feature countries

--- src/flutterwave-dotnet/APIs/Interfaces/IBanks.cs
+++ src/flutterwave-dotnet/APIs/Interfaces/IBanks.cs
@@ -6,10 +6,9 @@
         /// Get all Banks
         /// </summary>
         /// <param name="country">
-        /// Pass either NG, GH, KE, UG, ZA or TZ to get list of banks in Nigeria, Ghana, 
-        /// Kenya, Uganda, South Africa or Tanzania respectively
+        /// Get list of banks in this country
         /// </param>
         /// <returns>A list of Banks</returns>
-        public GetBanksResponse GetBanks(string country);
+        public GetBanksResponse GetBanks(Country country);
     }
 }
