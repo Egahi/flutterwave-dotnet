87f614f9aec7d4dc0c9e9f3284e1458f953146c6
Egahi S. O <okworisamuel6@gmail.com>
2020-10-19 17:02:21 +0100

    feature payment plans unit tests

--- src/flutterwave-dotnet-test/Apis/PaymentPlansTests.cs
+++ src/flutterwave-dotnet-test/Apis/PaymentPlansTests.cs
@@ -19,6 +19,163 @@ namespace flutterwave_dotnet_test.Apis
         }
 
         [Fact]
+        public void CancelPaymentPlan_InvalidSecretKey_ReturnsError()
+        {
+            // Arrange
+            int paymenPlanId = AppConstants.INVALID_PAYMENT_PLAN_ID;
+
+            var flutterwaveSecretKey = "";
+            _paymentPlans = new PaymentPlans(new FlutterwaveApi(flutterwaveSecretKey));
+
+            // Act
+            var result = _paymentPlans.CancelPaymentPlan(paymenPlanId);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<CancelPaymentPlanResponse>(result);
+            Assert.Equal(expected: AppConstants.ERROR_STATUS, actual: result.Status);
+            Assert.Equal(expected: AppConstants.INVALID_AUTHORIZATION_KEY_ERROR_MESSAGE, actual: result.Message);
+            Assert.Null(result.Data);
+        }
+
+        [Fact]
+        public void CancelPaymentPlan_ValidSecretKey_InvalidId_ReturnsError()
+        {
+            // Arrange
+            int paymentPlanId = AppConstants.INVALID_PAYMENT_PLAN_ID;
+
+            // Act
+            var result = _paymentPlans.CancelPaymentPlan(paymentPlanId);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<CancelPaymentPlanResponse>(result);
+            Assert.Equal(expected: AppConstants.ERROR_STATUS, actual: result.Status);
+            Assert.Equal(expected: AppConstants.CANCEL_PAYMENT_PLAN_ERROR_MESSAGE, actual: result.Message);
+            Assert.Null(result.Data);
+        }
+
+        [Fact]
+        public void CancelPaymentPlan_ValidSecretKey_ValidId_ReturnsPaymentPlan()
+        {
+            // Arrange
+            // Create a Payment Plan to test with
+            var createTestPaymentPlanReponse = CreatePaymentPlan();
+            int paymentPlanId = createTestPaymentPlanReponse.Data.Id;
+
+            // Act
+            var result = _paymentPlans.CancelPaymentPlan(paymentPlanId);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<CancelPaymentPlanResponse>(result);
+            Assert.Equal(expected: AppConstants.SUCCESS_STATUS, actual: result.Status);
+            Assert.Equal(expected: AppConstants.CANCEL_PAYMENT_PLAN_SUCCESS_MESSAGE, actual: result.Message);
+            Assert.IsType<PaymentPlan>(result.Data);
+            Assert.Equal(expected: paymentPlanId, actual: result.Data.Id);
+            Assert.Equal(expected: AppConstants.CANCELLED, actual: result.Data.Status);
+        }
+
+        [Fact]
+        public void CreatePaymentPlan_InvalidSecretKey_ReturnsError()
+        {
+            // Arrange
+            decimal amount = 5000;
+            string name = AppConstants.SAMPLE_PAYMENT_PLAN_NAME;
+            int duration = 24;
+
+            var flutterwaveSecretKey = "";
+            _paymentPlans = new PaymentPlans(new FlutterwaveApi(flutterwaveSecretKey));
+
+            // Act
+            var result = _paymentPlans.CreatePaymentPlan(amount, name, Interval.Monthly, duration);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<CreatePaymentPlanResponse>(result);
+            Assert.Equal(expected: AppConstants.ERROR_STATUS, actual: result.Status);
+            Assert.Equal(expected: AppConstants.INVALID_AUTHORIZATION_KEY_ERROR_MESSAGE,
+                actual: result.Message);
+            Assert.Null(result.Data);
+        }
+
+        [Fact]
+        public void CreatePaymentPlan_ValidSecretKey_ValidParamters_ReturnsPaymentPlan()
+        {
+            // Arrange
+            decimal amount = 5000;
+            string name = AppConstants.SAMPLE_PAYMENT_PLAN_NAME;
+            int duration = 24;
+
+            // Act
+            var result = _paymentPlans.CreatePaymentPlan(amount, name, Interval.Monthly, duration);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<CreatePaymentPlanResponse>(result);
+            Assert.Equal(expected: AppConstants.SUCCESS_STATUS, actual: result.Status);
+            Assert.Equal(expected: AppConstants.CREATE_PAYMENT_PLAN_SUCCESS_MESSAGE, actual: result.Message);
+            Assert.IsType<PaymentPlan>(result.Data);
+        }
+
+        [Fact]
+        public void GetPaymentPlan_InvalidSecretKey_ReturnsError()
+        {
+            // Arrange
+            int paymentPlanId = AppConstants.INVALID_PAYMENT_PLAN_ID;
+             
+            var flutterwaveSecretKey = "";
+            _paymentPlans = new PaymentPlans(new FlutterwaveApi(flutterwaveSecretKey));
+
+            // Act
+            var result = _paymentPlans.GetPaymentPlan(paymentPlanId);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<GetPaymentPlanResponse>(result);
+            Assert.Equal(expected: AppConstants.ERROR_STATUS, actual: result.Status);
+            Assert.Equal(expected: AppConstants.INVALID_AUTHORIZATION_KEY_ERROR_MESSAGE, actual: result.Message);
+            Assert.Null(result.Data);
+        }
+
+        [Fact]
+        public void GetPaymentPlan_ValidSecretKey_InvalidPaymentPlansId_ReturnsError()
+        {
+            // Arrange
+            int paymentPlanId = AppConstants.INVALID_PAYMENT_PLAN_ID;
+
+            // Act 
+            var result = _paymentPlans.GetPaymentPlan(paymentPlanId);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<GetPaymentPlanResponse>(result);
+            Assert.Equal(expected: AppConstants.ERROR_STATUS, actual: result.Status);
+            Assert.Equal(expected: AppConstants.GET_PAYMENT_PLAN_ERROR_MESSAGE, actual: result.Message);
+            Assert.Null(result.Data);
+        }
+        
+        [Fact]
+        public void GetPaymentPlan_ValidSecretKey_ValidPaymentPlanId_ReturnsPaymentPlan()
+        {
+            // Arrange
+            // Create a payment plan to test with
+            var createTestPaymentPlanReponse = CreatePaymentPlan();
+            int paymentPlanId = createTestPaymentPlanReponse.Data.Id;
+
+            // Act
+            var result = _paymentPlans.GetPaymentPlan(paymentPlanId);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<GetPaymentPlanResponse>(result);
+            Assert.Equal(expected: AppConstants.SUCCESS_STATUS, actual: result.Status);
+            Assert.Equal(expected: AppConstants.GET_PAYMENT_PLAN_SUCCESS_MESSAGE, actual: result.Message);
+            Assert.IsType<PaymentPlan>(result.Data);
+            Assert.Equal(expected: paymentPlanId, actual: result.Data.Id);
+        }
+
+        [Fact]
         public void GetPaymentPlans_InvalidSecretKey_ReturnsError()
         {
             // Arrange
@@ -42,129 +199,21 @@ namespace flutterwave_dotnet_test.Apis
         {
             // Act
             var result = _paymentPlans.GetPaymentPlans();
-             
+
             // Assert
             Assert.NotNull(result);
             Assert.IsType<GetPaymentPlansResponse>(result);
             Assert.Equal(expected: AppConstants.SUCCESS_STATUS, actual: result.Status);
-            Assert.Equal(expected: AppConstants.GET_PAYMENTPLANS_SUCCESS_MESSAGE, actual: result.Message);
+            Assert.Equal(expected: AppConstants.GET_PAYMENT_PLANS_SUCCESS_MESSAGE, actual: result.Message);
             Assert.IsType<List<PaymentPlan>>(result.Data);
-        }
-
-        [Fact]
-        public void GetPaymentPlan_InvalidSecretKey_ReturnsError()
-        {
-            // Arrange
-            int paymentPlanId = AppConstants.INVALID_PAYMENTPLAN_ID;
-             
-            var flutterwaveSecretKey = "";
-            _paymentPlans = new PaymentPlans(new FlutterwaveApi(flutterwaveSecretKey));
-
-            // Act
-            var result = _paymentPlans.GetPaymentPlan(paymentPlanId);
-
-            // Assert
-            Assert.NotNull(result);
-            Assert.IsType<GetPaymentPlanResponse>(result);
-            Assert.Equal(expected: AppConstants.ERROR_STATUS, actual: result.Status);
-            Assert.Equal(expected: AppConstants.INVALID_AUTHORIZATION_KEY_ERROR_MESSAGE, actual: result.Message);
-            Assert.Null(result.Data);
-        }
-
-        [Fact]
-        public void GetPaymentPlan_ValidSecretKey_InvalidPaymentPlansId_ReturnsError()
-        {
-            // Arrange
-            int paymentPlanId = AppConstants.INVALID_PAYMENTPLAN_ID;
-
-            // Act 
-            var result = _paymentPlans.GetPaymentPlan(paymentPlanId);
-
-            // Assert
-            Assert.NotNull(result);
-            Assert.IsType<GetPaymentPlanResponse>(result);
-            Assert.Equal(expected: AppConstants.ERROR_STATUS, actual: result.Status);
-            Assert.Equal(expected: AppConstants.GET_PAYMENTPLAN_ERROR_MESSAGE, actual: result.Message);
-            Assert.Null(result.Data);
-        }
-        
-        [Fact]
-        public void GetPaymentPlan_ValidSecretKey_ValidPaymentPlanId_ReturnsPaymentPlan()
-        {
-            // Arrange
-            // Create a payment plan to test with
-            var createTestPaymentPlanReponse = CreatePaymentPlan();
-            int paymentPlanId = createTestPaymentPlanReponse.Data.Id;
-
-            // Act
-            var result = _paymentPlans.GetPaymentPlan(paymentPlanId);
-
-            // Assert
-            Assert.NotNull(result);
-            Assert.IsType<GetPaymentPlanResponse>(result);
-            Assert.Equal(expected: AppConstants.SUCCESS_STATUS, actual: result.Status);
-            Assert.Equal(expected: AppConstants.GET_PAYMENTPLAN_SUCCESS_MESSAGE, actual: result.Message);
-            Assert.IsType<PaymentPlan>(result.Data);
-
-            // Cancel test Payment Plan
-            // for purposing of re-creating in future test runs
-            CancelTestPaymentPlan(createTestPaymentPlanReponse.Data.Id);
-        }
-
-        [Fact]
-        public void CreatePaymentPlan_InvalidSecretKey_ReturnsError()
-        {
-            // Arrange
-            decimal amount = AppConstants.PAYMENTPLAN_AMOUNT;
-            string name = AppConstants.PAYMENTPLAN_NAME;
-            decimal duration = AppConstants.PAYMENTPLAN_DURATION;
-            
-            var flutterwaveSecretKey = "";
-            _paymentPlans = new PaymentPlans(new FlutterwaveApi(flutterwaveSecretKey));
-
-            // Act
-            var result = _paymentPlans.CreatePaymentPlan(amount, name, Interval.Monthly, duration);
-
-            // Assert
-            Assert.NotNull(result);
-            Assert.IsType<CreatePaymentPlanResponse>(result);
-            Assert.Equal(expected: AppConstants.ERROR_STATUS, actual: result.Status);
-            Assert.Equal(expected: AppConstants.INVALID_AUTHORIZATION_KEY_ERROR_MESSAGE,
-                actual: result.Message);
-            Assert.Null(result.Data);
-        }
-
-        [Fact]
-        public void CreatePaymentPlan_ValidParamters_ReturnsPaymentPlan()
-        {
-            // Arrange
-            decimal amount = AppConstants.PAYMENTPLAN_AMOUNT;
-            string name = AppConstants.PAYMENTPLAN_NAME;
-            decimal duration = AppConstants.PAYMENTPLAN_DURATION;
-
-            // Act
-            var result = _paymentPlans.CreatePaymentPlan(amount, name, Interval.Monthly, duration);
-
-            // Assert
-            Assert.NotNull(result);
-            Assert.IsType<CreatePaymentPlanResponse>(result);
-            Assert.Equal(expected: AppConstants.SUCCESS_STATUS, actual: result.Status);
-            Assert.Equal(expected: AppConstants.CREATE_PAYMENTPLAN_SUCCESS_MESSAGE, actual: result.Message);
-            Assert.IsType<PaymentPlan>(result.Data);
-
-            // Cancel Payment Plan
-            // for purposing of re-creating in future test runs
-            CancelTestPaymentPlan(result.Data.Id);
         }
 
         [Fact]
         public void UpdatePaymentPlans_InvalidSecretKey_ReturnsError()
         {
             // Arrange
-            int paymentPlanId = AppConstants.INVALID_PAYMENTPLAN_ID;
-
-            string name = "January neighbourhood";
-            string status = "active";
+            int paymentPlanId = AppConstants.INVALID_PAYMENT_PLAN_ID;
+            string name = AppConstants.SAMPLE_NEW_PAYMENT_PLAN_NAME;
 
             var flutterwaveSecretKey = "";
             _paymentPlans = new PaymentPlans(new FlutterwaveApi(flutterwaveSecretKey));
@@ -172,7 +221,7 @@ namespace flutterwave_dotnet_test.Apis
             // Act
             var result = _paymentPlans.UpdatePaymentPlan(paymentPlanId,
                                                           name,
-                                                          status);
+                                                          Status.Active);
 
             // Assert
             Assert.NotNull(result);
@@ -185,118 +234,55 @@ namespace flutterwave_dotnet_test.Apis
         public void UpdatePaymentPlan_ValidSecretKey_InvalidPaymentPlanId_ReturnsError()
         {
             // Arrange
-            int paymentPlanId = AppConstants.INVALID_PAYMENTPLAN_ID;
-
-            string name = "January neighbourhood";
-            string status = "active";
+            int paymentPlanId = AppConstants.INVALID_PAYMENT_PLAN_ID;
+            string name = AppConstants.SAMPLE_NEW_PAYMENT_PLAN_NAME;
 
             // Act
             var result = _paymentPlans.UpdatePaymentPlan(paymentPlanId,
                                                           name,
-                                                          status);
+                                                          Status.Active);
 
             // Assert
             Assert.NotNull(result);
             Assert.IsType<UpdatePaymentPlanResponse>(result);
             Assert.Equal(expected: AppConstants.ERROR_STATUS, actual: result.Status);
-            Assert.Equal(expected: AppConstants.UPDATE_PAYMENTPLAN_ERROR_MESSAGE, actual: result.Message);
+            Assert.Equal(expected: AppConstants.UPDATE_PAYMENT_PLAN_ERROR_MESSAGE, actual: result.Message);
         }
 
         [Fact]
-        public void UpdatePaymentPlan_ValidParamters_ReturnsPaymentPlan()
+        public void UpdatePaymentPlan_ValidSecretKey_ValidParamters_ReturnsPaymentPlan()
         {
             // Arrange
             // Create a payment plan to test with
             var createTestPaymentPlanReponse = CreatePaymentPlan();
             int paymentPlanId = createTestPaymentPlanReponse.Data.Id;
-
-            string name = "March neighbourhood";
-            string status = "active";
+            string name = AppConstants.SAMPLE_NEW_PAYMENT_PLAN_NAME;
 
             // Act
             var result = _paymentPlans.UpdatePaymentPlan(paymentPlanId,
                                                           name,
-                                                          status);
+                                                          Status.Active);
 
             // Assert
             Assert.NotNull(result);
             Assert.IsType<UpdatePaymentPlanResponse>(result);
             Assert.Equal(expected: AppConstants.SUCCESS_STATUS, actual: result.Status);
-            Assert.Equal(expected: AppConstants.UPDATE_PAYMENTPLAN_SUCCESS_MESSAGE, actual: result.Message);
+            Assert.Equal(expected: AppConstants.UPDATE_PAYMENT_PLAN_SUCCESS_MESSAGE, actual: result.Message);
             Assert.IsType<PaymentPlan>(result.Data);
+            Assert.Equal(expected: paymentPlanId, actual: result.Data.Id);
             Assert.Equal(expected: name, actual: result.Data.Name);
-            Assert.Equal(expected: status, actual: result.Data.Status);
-
-            // Cancel test Payment Plan
-            // for purposing of re-creating in future test runs
-            CancelTestPaymentPlan(createTestPaymentPlanReponse.Data.Id);
-        }
-
-        [Fact]
-        public void CancelPaymentPlan_InvalidSecretKey_ReturnsError()
-        {
-            // Arrange
-            int paymenPlanId = AppConstants.INVALID_PAYMENTPLAN_ID;
-
-            var flutterwaveSecretKey = "";
-            _paymentPlans = new PaymentPlans(new FlutterwaveApi(flutterwaveSecretKey));
-
-            // Act
-            var result = _paymentPlans.CancelPaymentPlan(paymenPlanId);
-
-            // Assert
-            Assert.NotNull(result);
-            Assert.IsType<CancelPaymentPlanResponse>(result);
-            Assert.Equal(expected: AppConstants.ERROR_STATUS, actual: result.Status);
-            Assert.Equal(expected: AppConstants.INVALID_AUTHORIZATION_KEY_ERROR_MESSAGE, actual: result.Message);
-            Assert.Null(result.Data);
-        }
-        
-        [Fact]
-        public void CancelPaymentPlan_InvalidId_ReturnsError()
-        {
-            // Arrange
-            int paymentPlanId = AppConstants.INVALID_PAYMENTPLAN_ID;
-
-            // Act
-            var result = _paymentPlans.CancelPaymentPlan(paymentPlanId);
-
-            // Assert
-            Assert.NotNull(result);
-            Assert.IsType<CancelPaymentPlanResponse>(result);
-            Assert.Equal(expected: AppConstants.ERROR_STATUS, actual: result.Status);
-            Assert.Equal(expected: AppConstants.CANCEL_PAYMENTPLAN_ERROR_MESSAGE, actual: result.Message);
-            Assert.Null(result.Data);
-        }
-        
-        [Fact]
-        public void CancelPaymentPlan_ValidId_ReturnsSuccessMessage()
-        {
-            // Arrange
-            // Create a Payment Plan to test with
-            var createTestPaymentPlanReponse = CreatePaymentPlan();
-            int paymentPlanId = createTestPaymentPlanReponse.Data.Id;
-
-            // Act
-            var result = _paymentPlans.CancelPaymentPlan(paymentPlanId);
-
-            // Assert
-            Assert.NotNull(result);
-            Assert.IsType<CancelPaymentPlanResponse>(result);
-            Assert.Equal(expected: AppConstants.SUCCESS_STATUS, actual: result.Status);
-            Assert.Equal(expected: AppConstants.CANCEL_PAYMENTPLAN_SUCCESS_MESSAGE, actual: result.Message);
-            Assert.IsType<PaymentPlan>(result.Data);
+            Assert.Equal(expected: AppConstants.ACTIVE, actual: result.Data.Status);
         }
 
         /// <summary>
-        /// Create test Payment Paln for unit tests
+        /// Create test Payment Plan for unit tests
         /// </summary>
         /// <returns></returns>
         private CreatePaymentPlanResponse CreatePaymentPlan()
         {
             decimal amount = 5000;
-            string name = "Monthly Nepa Bill Collection";
-            decimal duration = 24;
+            string name = AppConstants.SAMPLE_PAYMENT_PLAN_NAME;
+            int duration = 24;
 
             var result = _paymentPlans.CreatePaymentPlan(amount,
                                                          name,
@@ -304,15 +290,6 @@ namespace flutterwave_dotnet_test.Apis
                                                          duration);
 
             return result;
-        }
-
-        /// <summary>
-        /// Delete test payment plan
-        /// </summary>
-        /// <returns></returns>
-        private void CancelTestPaymentPlan(int paymentPlanId)
-        {
-            _paymentPlans.CancelPaymentPlan(paymentPlanId);
         }
     }
 }
