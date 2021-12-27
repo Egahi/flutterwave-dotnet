ca5f689c5863a0cecb10bdd43f60f05a7a0cf9de
Egahi S. O <samuelokwori@microsoft.com>
2021-12-27 07:15:06 +0100

    add charge model

---
+++ src/flutterwave-dotnet/APIs/Implementations/Charge.cs
@@ -0,0 +1,19 @@
+﻿namespace Flutterwave.Net
+{
+  public class Charge
+  {
+    private FlutterwaveApi _flutterwaveApi { get; }
+
+    public Charge(FlutterwaveApi flutterwaveApi)
+    {
+      _flutterwaveApi = flutterwaveApi;
+    }
+
+    public void ChargeCard(decimal amount,
+                           Currency currency = Currency.NigerianNaira
+      )
+    {
+
+    }
+  }
+}
