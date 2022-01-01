d7aa2f2bff4ca7ff029bc78ee2bb7fd243dabdc8
Egahi S. O <samuelokwori@microsoft.com>
2022-01-01 21:06:09 +0100

    refactor - decorate class memmbers with json property names

--- src/flutterwave-dotnet/Responses/GetTransactionsResponse.cs
+++ src/flutterwave-dotnet/Responses/GetTransactionsResponse.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace Flutterwave.Net
 {
     public class GetTransactionsResponse : Response<List<Transaction>>
     {
+        [JsonProperty("meta")]
         public Meta Meta { get; set; }
     }
 }
